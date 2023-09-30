99fbb1a0a071e0db3e75dda0ea5b175dae4e22eb
Jênifer Vieira Goedert <91765173+Jenifer19IFC@users.noreply.github.com>
2023-09-30 14:15:51 -0300

    Update teste_mysql.php

--- meu-projeto/php/teste_mysql.php
+++ meu-projeto/php/teste_mysql.php
@@ -1,28 +1,19 @@
 <?php
-$servername = "mysql"; // Este é o nome do serviço MySQL no Docker Compose
+$servername = "mysql"; 
 $username = "test";
 $password = "jenifer123";
-$database = "bancoteste"; // Nome do seu banco de dados
+$database = "bancoteste"; 
 
-// Cria uma conexão
 $conn = new mysqli($servername, $username, $password, $database);
-
-// Verifica a conexão
 if ($conn->connect_error) {
 	die("Falha na conexão: " . $conn->connect_error);
 }
-
 echo "Conexão bem-sucedida ao MySQL.";
-
-// Query SQL para selecionar todos os registros da tabela "usuarios"
 $sql = "SELECT id, nome, email FROM usuarios";
 $result = $conn->query($sql);
 
 echo "<br><br>";
-
-// Verifica se a consulta retornou resultados
 if ($result->num_rows > 0) {
-	// Loop através dos resultados e exibe-os
 	while ($row = $result->fetch_assoc()) {
     	echo "ID: " . $row["id"]. " - Nome: " . $row["nome"]. " - Email: " . $row["email"]. "<br>";
 	}
@@ -30,7 +21,5 @@ if ($result->num_rows > 0) {
 	echo "Nenhum resultado encontrado.";
 }
 
-// Fecha a conexão
 $conn->close();
-echo "CONEXÃO FINALIZADA!";
 ?>
